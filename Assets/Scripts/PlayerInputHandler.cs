d298cd59031eb1aff48af342fbbfa4762ac48307
Dark Soul Studios <113238820+Dark-Soul9@users.noreply.github.com>
2024-12-31 19:32:22 +0500

    step 1

--- Assets/Scripts/PlayerInputHandler.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class PlayerInputHandler : MonoBehaviour
-{
-    public void SubmitPlayerChoice(int choiceIndex)
-    {
-        GameManager.Instance.PlayerMadeChoice(choiceIndex);
-    }
-}
